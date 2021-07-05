0ca6899fccb0f3a402b2b94d8ecf7257ea37e31a
Pietermachiel <pieter@roozen.nl>
2021-07-05 15:33:20 +0200

    document

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,9 @@
 import Header from "./header";
 import Footer from "./footer";
-import Meta from "./meta";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Meta />
       <Header />
       <div className="">{children}</div>
       <Footer />
