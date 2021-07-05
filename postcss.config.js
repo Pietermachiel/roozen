1ca9c1cb06a5008d1d914f03a5e26c104a878a40
Pietermachiel <pieter@roozen.nl>
2021-07-05 19:38:57 +0200

    c

--- postcss.config.js
+++ postcss.config.js
@@ -1,42 +1,42 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
 
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// };
-
-
 module.exports = {
-	plugins: [
-		'tailwindcss',
-		'autoprefixer',
-		'postcss-flexbugs-fixes',
-		[
-			'postcss-preset-env',
-			{
-				autoprefixer: {
-					flexbox: 'no-2009'
-				},
-				stage: 3,
-				features: {
-					'custom-properties': false
-				}
-			}
-		],
-		[
-			'@fullhuman/postcss-purgecss',
-			{
-				content: [
-					'./pages/**/*.{js,jsx,ts,tsx}',
-					'./components/**/*.{js,jsx,ts,tsx}'
-				],
-				defaultExtractor: (content) =>
-					content.match(/[\w-/:]+(?<!:)/g) || [],
-				safelist: ['html', 'body']
-			}
-		]
-	]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
+
+
+// module.exports = {
+// 	plugins: [
+// 		'tailwindcss',
+// 		'autoprefixer',
+// 		'postcss-flexbugs-fixes',
+// 		[
+// 			'postcss-preset-env',
+// 			{
+// 				autoprefixer: {
+// 					flexbox: 'no-2009'
+// 				},
+// 				stage: 3,
+// 				features: {
+// 					'custom-properties': false
+// 				}
+// 			}
+// 		],
+// 		[
+// 			'@fullhuman/postcss-purgecss',
+// 			{
+// 				content: [
+// 					'./pages/**/*.{js,jsx,ts,tsx}',
+// 					'./components/**/*.{js,jsx,ts,tsx}'
+// 				],
+// 				defaultExtractor: (content) =>
+// 					content.match(/[\w-/:]+(?<!:)/g) || [],
+// 				safelist: ['html', 'body']
+// 			}
+// 		]
+// 	]
+// };
