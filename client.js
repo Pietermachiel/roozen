6b29ae4f3963216e7ab8c862599a01ef98ea3806
Pietermachiel <pieter@roozen.nl>
2021-09-20 15:16:30 +0200

    env

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 import sanityClient from '@sanity/client';
 
 export default sanityClient({
-	projectId: 'xxtlwhgv',
-	dataset: 'production',
+	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
 	apiVersion: '2021-01-06',
 	useCdn: true
 });
